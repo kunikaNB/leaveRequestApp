d46232f26ed617a38b240c6840fbc1fd0a902d1e
Biyani <kunika.biyani@jp-n62vt7n291.intra.rakuten.co.jp>
2024-04-09 10:24:50 +0900

    Initial commit

---
+++ src/main/java/com/kunika/leaveRequestApp/LeaveRequestAppApplication.java
@@ -0,0 +1,13 @@
+package com.kunika.leaveRequestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveRequestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveRequestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kunika/leaveRequestApp/LeaveRequestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunika.leaveRequestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveRequestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
